029e02774d4690548bb925ef7bb8c29f7e48261c
Turbobent <kasp728h@edu.mercantec.dk>
2025-06-16 13:06:12 +0200

    can solve hard coded grid

---
+++ README.md
@@ -0,0 +1,5 @@
+this is a suduko solver
+
+compile: gcc (filename).c -o filename
+run: ./filename
+compile server: gcc server.c -o server.exe -lws2_32
--- main.c
+++ main.c
@@ -1,15 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define N 9
 
-void print(int array[N][N])
+/*print grid */
+void print(int arr[N][N])
 {
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N;j++){
-            printf("%d ", array[i][j],"\n");
-        }
+     for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            printf("%d ", arr[i][j]);
+        printf("\n");
     }
+}
+
+// Checks whether it will be legal
+// to assign num to the
+// given row, col
+int isSafe(int grid[N][N], int row, int col, int num)
+{
+
+    // Check if we find the same num
+    // in the similar row , we return 0
+    for (int x = 0; x <= 8; x++)
+        if (grid[row][x] == num)
+            return 0;
+
+    // Check if we find the same num in the
+    // similar column , we return 0
+    for (int x = 0; x <= 8; x++)
+        if (grid[x][col] == num)
+            return 0;
+
+    int startRow = row - row % 3, startCol = col - col % 3;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (grid[i + startRow][j + startCol] == num)
+                return 0;
+
+    return 1;
+}
+
+int solveSudoku(int grid[N][N], int row, int col)
+{
+    if (row == N - 1 && col == N)
+        return 1;
+
+    if (col == N) {
+        row++;
+        col = 0;
+    }
+
+    if (grid[row][col] > 0)
+        return solveSudoku(grid, row, col + 1);
+
+    for (int num = 1; num <= N; num++) {
+
+        if (isSafe(grid, row, col, num) == 1) {
+       
+            grid[row][col] = num;
+
+         
+            if (solveSudoku(grid, row, col + 1) == 1)
+                return 1;
+        }
+
+        grid[row][col] = 0;
+    }
+    return 0;
+}
+
+int generateValidGrid(){
+
 }
 
 int main(){
@@ -22,4 +85,11 @@ int main(){
                        { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
                        { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
                        { 0, 0, 0, 0, 8, 0, 0, 7, 9 } };
+
+    if (solveSudoku(grid, 0, 0) == 1)
+        print(grid);
+    else
+        printf("No solution exists");
+
+    return 0;
 }
